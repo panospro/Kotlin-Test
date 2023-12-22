a1f0e1835c0f70094c0671fe90db5f7e22958d81
protops <65860101+panospro@users.noreply.github.com>
2023-12-22 13:46:39 +0200

    Update SimpleInstrumentationTest.kt

--- app/src/androidTest/java/com/antonioleiva/weatherapp/SimpleInstrumentationTest.kt
+++ app/src/androidTest/java/com/antonioleiva/weatherapp/SimpleInstrumentationTest.kt
@@ -51,3 +51,37 @@ class SimpleInstrumentationTest {
                 }
             }
 }
+
+class SimpleInstrumentationTest2 {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test fun itemClickNavigatesToDetail() {
+        onView(withId(R.id.forecastList)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.weatherDescription))
+                .check(matches(isAssignableFrom(TextView::class.java)))
+    }
+
+    @Test fun modifyZipCodeChangesToolbarTitle() {
+        openActionBarOverflowOrOptionsMenu(activityRule.activity)
+        onView(withText(R.string.settings)).perform(click())
+        onView(withId(R.id.cityCode)).perform(replaceText("94301"))
+        pressBack()
+        onView(isAssignableFrom(Toolbar::class.java))
+                .check(matches(withToolbarTitle(`is`("Palo Alto (US)"))))
+    }
+
+    private fun withToolbarTitle(textMatcher: Matcher<CharSequence>): Matcher<Any> =
+            object : BoundedMatcher<Any, Toolbar>(Toolbar::class.java) {
+
+                override fun matchesSafely(toolbar: Toolbar): Boolean =
+                        textMatcher.matches(toolbar.title)
+
+                override fun describeTo(description: Description) {
+                    description.appendText("with toolbar title: ")
+                    textMatcher.describeTo(description)
+                }
+            }
+}
